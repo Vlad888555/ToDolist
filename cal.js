f7248f223e72d8fdb4c31f0e0ec46f84f6c1c83e
Vlad888555 <110382044+Vlad888555@users.noreply.github.com>
2023-11-07 09:39:54 +0600

    Add files via upload

--- cal.js
+++ cal.js
@@ -1,72 +1,152 @@
-const todoValue =  document.getElementById("todoText"),
-    listItems = document.getElementById("list-items"),
-    addUpdateClikc = document.getElementById("AddUpdateClick");
+const todoValue = document.getElementById("todoText");
+const listItems = document.getElementById("list-items");
+const addUpdateClick = document.getElementById("AddUpdateClick");
 let updateText;
-let todoData = JSON.parse(localStorage.getItem("todoData"));
-if(!todoData){
-    todoData=[];
-}
-ReadToDoItems();
-function ReadToDoItems(){
-    console.log(todoData);
-    todoData.forEach(element => {
-        let li = document.createElement("li");
-        let style = "";
-        if (element.status){
-            style = "style='text-decoration: line-through'";
-        }
-        const todoItems = ` <div ${style} ondblclick="CompleteTodoItem(this)>${element.item}</div>`;
-        li.innerHTML=todoItems;
-        listItems.appendChild(li);
+let todoData = JSON.parse(localStorage.getItem("todoData")) || [];
+
+const itemsPerPage = 5;
+let currentPage = 1;
+
+function SearchToDoItems() {
+    const searchValue = document.getElementById("searchText").value.toLowerCase();
+  
+    const filteredItems = todoData.filter((item) =>
+      item.item.toLowerCase().includes(searchValue)
+    );
+  
+    listItems.innerHTML = "";
+  
+    filteredItems.forEach((element, index) => {
+      let li = document.createElement("li");
+      const todoItem = document.createElement("div");
+      todoItem.innerText = element.item;
+  
+      const editButton = document.createElement("button");
+      editButton.innerText = "Изменить";
+      editButton.addEventListener("click", () => EditToDoItem(index));
+  
+      const deleteButton = document.createElement("button");
+      deleteButton.innerText = "Удалить";
+      deleteButton.addEventListener("click", () => DeleteToDoItem(index));
+  
+      li.appendChild(todoItem);
+      li.appendChild(editButton);
+      li.appendChild(deleteButton);
+  
+      if (element.status) {
+        todoItem.style.textDecoration = "line-through";
+      }
+  
+      listItems.appendChild(li);
     });
-}    
-    
-function CreateToDoDate(){
-    console.log(todoValue.value);
-    if (todoValue.value ===""){
-        alert("Введите хоть чтонибудь");
-        todoValue.focus();
-    }
+  }
+  
+  function EnableAddMode() {
+    todoValue.value = "";
+    addUpdateClick.innerText = "+";
+    addUpdateClick.removeEventListener("click", UpdateToDoItem);
+    addUpdateClick.addEventListener("click", CreateToDoItem);
+  }
+
+ReadToDoItems();
+
+function ReadToDoItems() {
+  listItems.innerHTML = "";
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = todoData.slice(startIndex, endIndex);
+
+  itemsToDisplay.forEach((element, index) => {
     let li = document.createElement("li");
-    const todoItems = `<div ondblclick="CompleteTodoItem(this)">${todoValue.value}</div><div><button class ="edit todo-control" onclick ="Updete(this)">Изменить</button><button calss ="delete todo-control" onclick="Delet(this)">Удалить</button></div>`;
-    
-    li.innerHTML = todoItems;
+    const todoItem = document.createElement("div");
+    todoItem.innerText = element.item; // добавляем текст из массива
+
+    const editButton = document.createElement("button");
+    editButton.innerText = "Изменить";
+    editButton.addEventListener("click", () => EditToDoItem(index));
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Удалить";
+    deleteButton.addEventListener("click", () => DeleteToDoItem(index));
+
+    li.appendChild(todoItem);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+
+    if (element.status) {
+      todoItem.style.textDecoration = "line-through";
+    }
+
     listItems.appendChild(li);
-    todoValue.value = "";
-
-    if(!todoData){
-        todoData=[];
-    }
-
-    let dataItem = {item: todoData.value, status: false};
-    todoData.push(dataItem);
-}
-function UpdateOnSelectionitems(){
-    todoValue.value = updateText.innerText;
-    addUpdateClikc.setAttribute("onclick", "UpdateOnSelectionitems()");
-    todoValue.value = "";
-}
-function CompleteTodoItem(e){
-    if(e.parentElement.querySelector("div").style.textDecoration===""){
-        e.parentElement.querySelector("div").style.textDecoration = "line-through";
-        todoData.forEach((element)=>{
-            if(e.parentElement.querySelector("div").innerText.trim() == element.item){
-                element.status = true;
-            }
-        })
-    }
+  });
+  document.getElementById(
+    "currentPage"
+  ).textContent = `Страница ${currentPage}`;
+  document.getElementById("searchText").addEventListener("input", SearchToDoItems);
 }
 
-function Updete(e){
-    if(e.parentElement.parentElement.querySelector("div").style.textDecoration === ""){
-    todoValue.value = (e.parentElement.parentElement.querySelector("div").textContent);
-    addUpdateClikc.setAttribute("onclick", "UpdateOnSelectionitems()");
-    }
+document.getElementById("prevPage").addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    ReadToDoItems();
+  }
+});
+
+document.getElementById("nextPage").addEventListener("click", () => {
+  const maxPage = Math.ceil(todoData.length / itemsPerPage);
+  if (currentPage < maxPage) {
+    currentPage++;
+    ReadToDoItems();
+  }
+});
+
+function CreateToDoItem() {
+  if (todoValue.value === "") {
+    alert("Введите хоть что-нибудь");
+    todoValue.focus();
+    return;
+  }
+
+  let dataItem = { item: todoValue.value, status: false };
+  todoData.push(dataItem);
+  saveTodoData();
+  ReadToDoItems();
+  todoValue.value = "";
 }
-function Delet(e){
-    let deleteValue = e.parentElement.parentElement.querySelector("div").innerText;
-    if(confirm("Точно хотите удалить")){
-        e.parentElement.parentElement.parentElement.querySelector("li").remove();
-    }
+
+function EditToDoItem(index) {
+  updateText = todoData[index];
+  todoValue.value = updateText.item;
+  addUpdateClick.innerText = "Изменить";
+  addUpdateClick.removeEventListener("click", CreateToDoItem);
+  addUpdateClick.addEventListener("click", () => UpdateToDoItem(index));
 }
+
+function UpdateToDoItem(index) {
+    if (todoValue.value === "") {
+    alert("Введите хоть что-нибудь");
+    todoValue.focus();
+    return;
+  }
     
+  todoData[index].item = todoValue.value;
+  saveTodoData();
+  addUpdateClick.innerText = "+";
+  addUpdateClick.removeEventListener("click", UpdateToDoItem);
+  addUpdateClick.addEventListener("click", CreateToDoItem);
+  todoValue.value = "";
+  ReadToDoItems();
+}
+
+function DeleteToDoItem(index) {
+  todoData.splice(index, 1);
+  saveTodoData();
+  ReadToDoItems();
+}
+
+function saveTodoData() {
+  localStorage.setItem("todoData", JSON.stringify(todoData));
+}
+
+addUpdateClick.addEventListener("click", CreateToDoItem);
